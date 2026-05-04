071b4773c3d0e935a575fec14ae0e2ea712fa1df
Eve <evecheng999@gmail.com>
2026-05-04 17:02:33 +1000

    continue

--- examples/harmonic_example.py
+++ examples/harmonic_example.py
@@ -7,8 +7,7 @@ def main():
     D = [[np.sqrt((x1-x2)**2 + (y1-y2)**2) for x2, y2 in pts] for x1, y1 in pts]
 
     hc = harmonic_cycle(D, cycle_dim=1, sim_log=True)
-    simplices, births = hc.rips_filtration(threshold=1.5)
-    hc.compute_harmonics(simplices, births)
+    hc.run_harmonics(threshold=1.5)
     hc.save_log()
 
 
--- src/network_lab_tda/tda_visualisation/simplicial_pyvis.py
+++ src/network_lab_tda/tda_visualisation/simplicial_pyvis.py
@@ -1,5 +1,5 @@
 #TODO add appears at for all cycles
-#separate the simplices and appears_at to a different function
+#separate the simplicies and appears_at to a different function
 
 #edit pyvis setup here
 def pyvis_setup():
@@ -42,20 +42,20 @@ def pyvis_setup():
 def read_cycles_from_jason(cycles_path):
     return None
 
-#Distance matrix: designed for Rips. but also accepts simplices
+#Distance matrix: designed for Rips. but also accepts simplicies
 class simplicial_pyvis:
-    def __init__(self, max_dim, distance_mat=None, simplices=None, cycles=None, cycle_dim=None, index_to_name=None, only_neiborhood=False):
-        if distance_mat is None and simplices is None:
-            raise ValueError("Provide either distance_mat or simplices.")
-        if distance_mat is not None and simplices is not None:
-            raise ValueError("Provide only one of distance_mat or simplices, not both.")
+    def __init__(self, max_dim, distance_mat=None, simplicies=None, cycles=None, cycle_dim=None, index_to_name=None, only_neiborhood=False):
+        if distance_mat is None and simplicies is None:
+            raise ValueError("Provide either distance_mat or simplicies.")
+        if distance_mat is not None and simplicies is not None:
+            raise ValueError("Provide only one of distance_mat or simplicies, not both.")
 
         self.D = distance_mat
-        #dictionary: "0": 0-simplcies, "1": 1-simplices
-        self.simplices = simplices
+        #dictionary: "0": 0-simplcies, "1": 1-simplicies
+        self.simplicies = simplicies
         self.max_dim = max_dim
         # --- cycle-related ---
-        #in the form of list of list of simplices, e.g: [[[1,4],[3,4]],...]
+        #in the form of list of list of simplicies, e.g: [[[1,4],[3,4]],...]
         self.cycles = cycles
         self.cycle_dim = cycle_dim
         self.only_neiborhood = only_neiborhood
@@ -73,28 +73,28 @@ class simplicial_pyvis:
    #this needs to be added first, because the package does not overwrite
     def add_cycles(self):
         if self.cycles == None: 
-            return self.net
+            return None
 
-        if cycle_dim == 1:
+        if self.cycle_dim == 1:
             for cycle in self.cycles:
                 for edge in cycle:
                     source, target = edge
-                    self.net.add_edge(source, target, value=G[source][target]["weight"], color="red") 
+                    self.net.add_edge(source, target, color="red") 
         else:
             print("Warning: add cycles is not well-defined for higher dimensional cycles")
 
     def add_graph_to_net(self):
         if self.D is not None:
-            nodes = range(D.shape[1])
-            rows, cols = np.where(D > 0)
+            nodes = range(self.D.shape[1])
+            rows, cols = np.where(np.triu(self.D) > 0)
             edge_pos = zip(rows.tolist(), cols.tolist())
             for node in nodes:
-                self.net.add_node(node,label=index_to_name[node])
+                self.net.add_node(node,label=self.index_to_name[node])
             for ind in edge_pos:
-                self.net.add_edge(ind[0],ind[1],width=1/(max(D[ind[0],ind[1]],1e-5)))
-        elif self.simplices is not None:
-            nodes = simplices["0"]
-            edges = simplices["1"]
+                self.net.add_edge(ind[0],ind[1],value=1/(max(D[ind[0],ind[1]],1e-5)))
+        elif self.simplicies is not None:
+            nodes = self.simplicies["0"]
+            edges = self.simplicies["1"]
             self.net.add_nodes(nodes)
             self.net.add_edges(edges)
 
