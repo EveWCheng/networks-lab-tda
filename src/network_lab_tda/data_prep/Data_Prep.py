65dd1ec881b6ad32138922c1b4a6eba086d9011c
Eve <evecheng999@gmail.com>
2026-06-23 18:31:27 +1000

    header stuff

--- src/network_lab_tda/data_prep/Data_Prep.py
+++ src/network_lab_tda/data_prep/Data_Prep.py
@@ -1,19 +1,41 @@
 import numpy as np
+import pandas as pd
+import networkx as nx
 import os
 
+
+
 class Data_Prep:
-    def __init__(self,filepath=None,matrix=None,G=None,log_path=None):
+    def __init__(self,filepath=None,matrix=None,G=None,log_path=None,headers=False,header_fn="header.txt"):
+        self.log_path = log_path or os.path.join(os.getcwd(), "outputs")
+        os.makedirs(self.log_path, exist_ok=True)
+ 
         if (filepath is None and matrix is None and G is None):
             print("You need to pass either a distance matrix or a filepath or a networkx object")
         elif filepath is not None:
-            self.matrix = np.loadtxt(filepath)
+            if headers:
+                df = pd.read_csv(filepath, sep=None, engine="python", header=0, index_col=0)
+                self.df = df
+                self.matrix = df.to_numpy(dtype=float)
+                headers_list = df.columns.tolist()
+                with open(os.path.join(self.log_path, header_fn), "w") as f:
+                    f.write("\n".join(headers_list))
+ 
+            else:
+                self.matrix = np.loadtxt(filepath)
         elif matrix is not None:
             self.matrix = matrix
         else:
             self.G = G
-
-        self.log_path = log_path or os.path.join(os.getcwd(), "outputs")
-        os.makedirs(os.path.dirname(self.log_path), exist_ok=True)
+        self.headers = headers
+        if G is None:
+            n = self.matrix.shape[0]
+            self.G = nx.Graph()
+            self.G.add_nodes_from(range(n))
+            for i in range(n):
+                for j in range(i + 1, n):
+                    if self.matrix[i,j]>0:
+                        self.G.add_edge(i, j, length=self.matrix[i, j])
 
 
 
--- src/network_lab_tda/data_prep/Populate_Edge.py
+++ src/network_lab_tda/data_prep/Populate_Edge.py
@@ -1,17 +1,24 @@
 import numpy as np
 import networkx as nx
 from pyvis.network import Network
-from math import floor
+import math
 import os
 import time
 
 from .Data_Prep import Data_Prep
 
 class Populate_Edge(Data_Prep):
-    def __init__(self,G,log_path=None,epsilon=None,vis=True,num_added = 0):
-        super().__init__(G=G,log_path=log_path)
+    def __init__(self,G,log_path=None,headers=False,header_fn="header.txt",populated_header_fn="populated_headers.txt",epsilon=None,vis=False,num_added = 0):
+        super().__init__(G=G,log_path=log_path,headers=headers,header_fn=header_fn)
+        if headers:
+            headers_path = os.path.join(self.log_path, header_fn)
+            with open(headers_path, "r") as f:
+                self.headers_list = [line.strip() for line in f.readlines()]
+            self.node_labels = dict(enumerate(self.headers_list))
+        else:
+            self.node_labels = {i: i for i in range(G.number_of_nodes())}
         if epsilon == None:
-            self.epsilon = min(d for _, _, d in G.edges(data='length'))
+            self.epsilon = np.percentile([d for _, _, d in G.edges(data='length')], 25)
         else:
             print(f"Epsilon is set to a custom value {epsilon}")
             self.epsilon = epsilon
@@ -19,6 +26,7 @@ class Populate_Edge(Data_Prep):
         self.original_node_count = G.number_of_nodes()
         self.vis = vis
         self.num_added = 0
+        self.populated_header_fn = populated_header_fn
  
 
     def visualise(self,name):
@@ -32,7 +40,7 @@ class Populate_Edge(Data_Prep):
         net.write_html(os.path.join(self.log_path,name))
 
     def add_nodes_to_one_edge(self,u,v,length):
-        number_nodes = floor(length/self.epsilon)-(1 if length % self.epsilon == 0 else 0)
+        number_nodes = math.floor(length/self.epsilon)-(1 if math.isclose(length % self.epsilon, 0) else 0)
         if number_nodes > 1:
             last_edge_weight = length - number_nodes * self.epsilon
             max_index = self.max_index
@@ -41,6 +49,7 @@ class Populate_Edge(Data_Prep):
             for number in range(number_nodes):
                 node_name = max_index+number+1
                 self.G.add_node(node_name)
+                self.node_labels[node_name] = node_name
                 extra_nodes.append(node_name)
 
             self.G.add_edge(u,extra_nodes[0],length=self.epsilon)
@@ -60,6 +69,9 @@ class Populate_Edge(Data_Prep):
             length = self.G.edges[e]['length']
             self.add_nodes_to_one_edge(u,v,length)
         print(f"{self.num_added} nodes added")
+
+        with open(os.path.join(self.log_path, self.populated_header_fn), "w") as f:
+            f.write("\n".join(str(v) for v in self.node_labels.values()))
 
         dist_matrix = nx.floyd_warshall_numpy(self.G, weight='length')
         np.savetxt(os.path.join(self.log_path,"populated_distance_matrix.txt"), dist_matrix)
